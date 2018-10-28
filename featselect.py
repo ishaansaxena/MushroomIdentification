8c2a690b116fc1b22f9c7ad8fbb2b2fa45e729b6
Swaraj Bhaduri <sbhadur@purdue.edu>
2018-10-28 16:43:28 -0400

    Feature Select

---
+++ featselect.py
@@ -0,0 +1,46 @@
+import data
+import sol_greedysubset as gr
+import sol_forwardfitting as ff
+import sol_myopicfitting as mp
+import project as pr
+import matplotlib.pyplot as plt
+
+def run(F):
+	c_X, c_Y = data.load(pr.config['filename'],pr.config['label'])
+	
+	X = c_X.values
+
+	#ForwardFitting
+	t1, t1_S = ff.run(F,X,c_Y)
+	
+	#Greedy Subset
+	t2, t2_S = gr.run(F,X,c_Y)
+
+	#MyopicFitting
+	t3, t3_S = gr.run(F,X,c_Y)
+
+	#Plot Greedy Subset
+	plt.figure()
+	plt.bar(range(t2_S.size), t2_S)
+	plt.xlabel('Feature')
+	plt.ylabel('Weight')
+	plt.title('Greedy Subset')
+	plt.show()
+
+	#Plot Forward Fitting
+	plt.figure()
+	plt.bar(range(t1_S.size), t1_S)
+	plt.xlabel('Feature')
+	plt.ylabel('Weight')
+	plt.title('Forward Fitting')
+	plt.show()
+
+	#Plot Myopic Subset
+	plt.figure()
+	plt.bar(range(t3_S.size), t3_S)
+	plt.xlabel('Feature')
+	plt.ylabel('Weight')
+	plt.title('Myopic Fitting')
+	plt.show()
+
+run(22)
