3164c2e1ed6b4c670d31639a9bd0dd3cb5a67599
Swaraj Bhaduri <sbhadur@purdue.edu>
2018-10-28 18:42:12 -0400

    Feature Select - Forward Fit Positives Problem

--- sol_forwardfitting.py
+++ sol_forwardfitting.py
@@ -1,66 +1,50 @@
+import linreg as lr
+import sys
 import numpy as np
 
-import linreg
-
 # Input: number of features F
-#        numpy matrix X of features, with n rows (samples), d columns (features)
-#           X[i, j] is the j-th feature of the i-th sample
-#        numpy vector y of scalar values, with n rows (samples), 1 column
-#           y[i] is the scalar value of the i-th sample
+# numpy matrix X of features, with n rows (samples), d columns (features)
+# X[i,j] is the j-th feature of the i-th sample
+# numpy vector y of scalar values, with n rows (samples), 1 column
+# y[i] is the scalar value of the i-th sample
 # Output: numpy vector of selected features S, with F rows, 1 column
-#         numpy vector thetaS, with F rows, 1 column
-#           thetaS[0] corresponds to the weight of feature S[0]
-#           thetaS[1] corresponds to the weight of feature S[1]
-#           and so on and so forth
-def run(F, X, y):
-    # Get n, d from data
-    (n, d) = X.shape
-
-    # Initialize S as an empty set
+# numpy vector thetaS, with F rows, 1 column
+# thetaS[0] corresponds to the weight of feature S[0]
+# thetaS[1] corresponds to the weight of feature S[1]
+# and so on and so forth
+def run(F,X,y):
     S = []
     thetaS = []
-    # For f in [0, F - 1]:
+    z = np.zeros((len(X),1))
     for f in range(F):
-        # Get all the z values
-        if len(S) == 0:
-            z = y
-        else:
-            tS = np.array(thetaS).reshape((f, 1))
-            z = y - X[:, S].dot(tS)
+        mins = float("inf")
+        minsum = float("inf")
+        thetagucci = 0
+        for t in range(len(X)):
+            inside = [i for i in xrange(len(X[0])) if i in S]
+            data = X[:, inside]     
+            if (len(S) == 0):
+                z[t] = y[t]
+            else:
+                z[t] = y[t] - np.vdot(np.asarray(thetaS).reshape(len(thetaS), 1), data[t].reshape(1,len(thetaS)))
+                
+        for j in range(len(X[0])):
+            if j not in S:
+                data = X[:, j]
+                thetaj = lr.run(data.reshape(len(X),1), y)
 
-        # Initialize empty J
-        J = []
-        theta = []
+                sums = 0
+                for t in range(len(X)):
+                    sums += (z[t] - np.vdot(thetaj, data[t]))**2
+                sums = 0.5*sums
+                if (sums < minsum):
+                    minsum = sums
+                    mins = j
+                    thetagucci = thetaj
+        jhat = mins
+        S.append(jhat)
+        thetaS.append(thetagucci)
 
-        # For each j not in S
-        for j in range(d):
-            # If j is in the set already, skip j
-            if j in S:
-                J.append(float("inf"))
-                theta.append(0)
-                continue
+    return (np.asarray(S).reshape(F,1), np.asarray(thetaS).reshape(F,1))
+    
 
-            # Get theta value from linreg
-            theta_j = linreg.run(X[:, j].reshape((n, 1)), z)
-            theta.append(theta_j)
-
-            # Get J values from all t
-            J.append(np.sum([
-                ((z[t] - theta_j.dot(X[t, j])) ** 2)/2.0
-                for t in range(n)
-            ]))
-
-        # Get argmin for J
-        j = np.argmin(J)
-
-        # Add j to S
-        S.append(j)
-
-        # Add theta[j] to thetaS
-        thetaS.append(theta[j])
-
-    # Reshape to match criteria
-    S = np.array(S).reshape((F, 1))
-    thetaS = np.array(thetaS).reshape((F, 1))
-
-    return (S, thetaS)
