32873882c240fd27d05c040f5821b4f269249056
Swaraj Bhaduri <sbhadur@purdue.edu>
2018-11-16 17:28:02 -0500

    Greedy correct

--- featureSelection.py
+++ featureSelection.py
@@ -3,22 +3,37 @@ import sol_greedysubset as gr
 import project as pr
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+import pandas as pd
 
 def run(F):
     c_X, c_Y = data.load()
+    print(c_Y)
 
     X = c_X.values
     #Greedy Subset
     t2, t2_S = gr.run(F,X,c_Y)
+    print(X)
+    print(t2)
+    print(t2_S)
+    
+    df = pd.DataFrame({
+        'feature' : t2,
+        'weight' : t2_S,
+        'weightabs': np.abs(t2_S)
+        })
 
     #Plot Greedy Subset
     plt.figure()
-    plt.bar(range(t2_S.size), t2_S)
+    
+    result = df.groupby(["feature"])['weightabs'].aggregate(np.median).reset_index().sort_values('weightabs')
+
+    sns.barplot(x='feature', y="weight", data=df, order=result['feature'])
+
+    # df.groupby(['feature']).median().sort_values("weightabs").plot.bar()
     plt.xlabel('Feature')
     plt.ylabel('Weight')
     plt.title('Greedy Subset')
     plt.show()
-
-    return t2, t2_S
-
-run(50)
+    
+run(107)
